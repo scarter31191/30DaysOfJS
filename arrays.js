2e330207266af591fcc6e015b354f280d5eab303
scarter31191 <scarter31191@gmaile.com>
2021-08-22 23:35:03 -0400

    modified array practice

--- arrays.js
+++ arrays.js
@@ -14,3 +14,55 @@ const numbers = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 33]
 
 console.log('numbers:', numbers)
 console.log('numbers of numbers:', numbers.length)
+
+//array different data types
+
+const arr2 = [
+    'Asabeneh',
+    250,
+    true, //boolean
+    { country: 'Finland', city: 'Helsinki' }, //object
+    { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] }, //objects
+]
+console.log(arr2)
+
+// creating array using splits
+let js = 'Javacript'
+const charsInJavaScript = js.split('')
+
+console.log(charsInJavaScript)
+
+let companiesString = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+const companies = companiesString.split(',')
+
+console.log(companies) // ["Facebook", " Google", " Microsoft", " Apple", " IBM", " Oracle", " Amazon"]
+let txt =
+  'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+const words = txt.split(' ')
+
+console.log(words)
+// the text has special characters think how you can just get only the words
+// ["I", "love", "teaching", "and", "empowering", "people.", "I", "teach", "HTML,", "CSS,", "JS,", "React,", "Python"]
+
+// Accessing array items using index
+const fruits = ['banana', 'orange', 'mango', 'lemon']
+console.log(fruits[0])
+console.log(fruits[3])
+
+const numbers2 = [0, 3.14, 9.81, 37, 98.6, 100] // set of numbers
+
+console.log(numbers2.length) // => to know the size of the array, which is 6
+console.log(numbers2) // -> [0, 3.14, 9.81, 37, 98.6, 100]
+console.log(numbers2[0]) //  -> 0
+console.log(numbers2[5]) //  -> 100
+
+let lastIndex = numbers2.length - 1
+console.log(numbers2[lastIndex]) // -> 100
+
+// Modifying array element
+const numbers3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numbers3[0] = 23 //changed 1 which had the index of 0 to 23
+numbers3[9] = 100 //changed 10 which had the index of 9 to 100
+
+console.log(numbers3) // [23, 2, 3, 4, 5, 6, 7, 8, 9, 100]
